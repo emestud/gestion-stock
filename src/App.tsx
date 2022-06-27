f63235b2d87b74bb7a01fbc63e2d099e7c680f28
Vincent WENDLING <vincent@starling-burgers.com>
2022-06-27 13:45:40 +0200

    :tada: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+const App = () => {
+
+  return (
+    <div>
+      <h1 className="text-red-400">App</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
