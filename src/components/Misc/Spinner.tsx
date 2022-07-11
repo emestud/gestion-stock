a762b5fb462293e9905922a28a1d99b0dc5112c6
Vincent WENDLING <vincent@starling-burgers.com>
2022-07-11 18:29:42 +0200

    fix: changed the spinning animation

--- src/components/Misc/Spinner.tsx
+++ src/components/Misc/Spinner.tsx
@@ -1,9 +1,13 @@
+import { useEffect, useState } from 'react'
 import Spin from '../../assets/spinning-burger.gif'
 
 const Spinner = () => {
 
     return (
-        <img src={Spin} alt="spin loading" className="m-auto pt-24"/>
+        <div className="relative m-auto pt-24 flex flex-col justify-center items-center gap-12">
+            <img src={Spin} alt="spin loading" />
+            <h3 className='text-lg md:text-xl lg:text-2xl'>Chargement...</h3>
+        </div>
     )
 }
 
