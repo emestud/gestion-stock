8da6bae1b9b99e6198d740a9433c99b76ca7869c
Vincent WENDLING <vincent@starling-burgers.com>
2022-07-22 20:10:40 +0200

    fix: fixed some linting stuff in order-store.ts

--- src/stores/order-store.ts
+++ src/stores/order-store.ts
@@ -1,69 +1,73 @@
-import { getOrderByID, getWasteByID, getLastModificationOfOrder, 
-        getOrdersByDate, getAllOriginalOrders, getWastesByDate, getAllWastes, getRestaurantData, get3TupleFromOrder, getContainerNameByID, getItemByID } from "../databaseClient";
-import { LabItemInfo, Order, OrderTuple, Waste, ItemWithLabInfo } from "../types";
+import {
+  getOrderByID,
+  getWasteByID,
+  getLastModificationOfOrder,
+  getOrdersByDate,
+  getAllOriginalOrders,
+  getWastesByDate,
+  getAllWastes,
+  getRestaurantData,
+  get3TupleFromOrder,
+  getContainerNameByID,
+  getItemByID,
+} from '../databaseClient';
+import {LabItemInfo, Order, OrderTuple, Waste, ItemWithLabInfo} from '../types';
 
 export class OrderStore {
+  /**
+   * This function fetchs an returns an order from the DB
+   * @param orderID number representing the order ID
+   */
+  async getOrder(orderID: string, mode = 'Order') {
+    let order = null;
+    let lastModification = null;
 
-    /**
-     * This function fetchs an returns an order from the DB
-     * @param orderID number representing the order ID
-     */
-   async getOrder(orderID: string, mode: string = "Order"){
-
-      let order = null;
-      let lastModification = null;
-
-      if (mode === "Order") {
-        order = await getOrderByID(orderID);
-        lastModification = await getLastModificationOfOrder(orderID);
-      }
-      else {
-        order = await getWasteByID(orderID);
-      }
-
-      let originalOrder = null;
-      let lastModificationOrder = null;
-
-      if (order !== null) {
-        originalOrder = order;
-        if (lastModification !== null) {
-          lastModificationOrder = lastModification;
-        }
-        else {
-          lastModificationOrder = originalOrder
-        }
-
-      }
-      return [originalOrder, lastModificationOrder];
+    if (mode === 'Order') {
+      order = await getOrderByID(orderID);
+      lastModification = await getLastModificationOfOrder(orderID);
+    } else {
+      order = await getWasteByID(orderID);
     }
 
+    let originalOrder = null;
+    let lastModificationOrder = null;
+
+    if (order !== null) {
+      originalOrder = order;
+      if (lastModification !== null) {
+        lastModificationOrder = lastModification;
+      } else {
+        lastModificationOrder = originalOrder;
+      }
+    }
+    return [originalOrder, lastModificationOrder];
+  }
+
   public async getOrders(date: string | null) {
-    const orders = new Array();
+    const orders: Array<Order> = [];
 
     try {
       let data: Array<Order> | null;
       if (date !== null) {
+        let orders = await getOrdersByDate(date);
 
-          let orders = await getOrdersByDate(date);
-          
-          if (orders !== null) {
-            orders = orders.filter((order)=>{
-              return order.original_order === null;
-            })
-          }
-          data = orders;
-      }
-      else {
-          data = await getAllOriginalOrders();
+        if (orders !== null) {
+          orders = orders.filter(order => {
+            return order.original_order === null;
+          });
+        }
+        data = orders;
+      } else {
+        data = await getAllOriginalOrders();
       }
 
       if (data?.length !== 0 && data !== null) {
         orders.push(...data);
-        orders.sort((a:any, b:any)=>{
-          let dateA = new Date(a.created_at);
-          let dateB = new Date(b.created_at);
+        orders.sort((a: Order, b: Order) => {
+          const dateA = new Date(a.created_at);
+          const dateB = new Date(b.created_at);
           return dateB.getTime() - dateA.getTime();
-        })
+        });
       }
     } catch (err) {
       console.log(err);
@@ -72,26 +76,24 @@ export class OrderStore {
     return orders;
   }
 
-
   public async getWastes(date: string | null) {
-    const wastes = new Array();
+    const wastes: Array<Waste> = [];
 
     try {
       let data: Array<Waste> | null;
       if (date !== null) {
-          data = await getWastesByDate(date);
-      }
-      else {
-          data = await getAllWastes();
+        data = await getWastesByDate(date);
+      } else {
+        data = await getAllWastes();
       }
 
       if (data?.length !== 0 && data !== null) {
         wastes.push(...data);
-        wastes.sort((a:any, b:any)=>{
-          let dateA = new Date(a.created_at);
-          let dateB = new Date(b.created_at);
+        wastes.sort((a: Waste, b: Waste) => {
+          const dateA = new Date(a.created_at);
+          const dateB = new Date(b.created_at);
           return dateB.getTime() - dateA.getTime();
-        })
+        });
       }
     } catch (err) {
       console.log(err);
@@ -100,64 +102,57 @@ export class OrderStore {
     return wastes;
   }
 
-
   public async getModifiedOrders(orders: Array<Order>) {
-    let ordersTuples:Array<OrderTuple> = [];
+    const ordersTuples: Array<OrderTuple> = [];
 
     for (const order of orders) {
-
-      let modifiedOrder = await getLastModificationOfOrder(order.id);
+      const modifiedOrder = await getLastModificationOfOrder(order.id);
 
       if (modifiedOrder === null || modifiedOrder.length === 0) {
         ordersTuples.push({
           originalOrder: order,
-          modifiedOrder: order
+          modifiedOrder: order,
         }); // we push the exact same order if the order hasnt been modified
-      }
-      else {
+      } else {
         ordersTuples.push({
-          originalOrder: order, 
-          modifiedOrder: modifiedOrder});
+          originalOrder: order,
+          modifiedOrder: modifiedOrder,
+        });
       }
-
-    }    
+    }
     return ordersTuples;
   }
 
   public async prepareOrders(orders: Order[]) {
+    const orderItems = [];
 
-    const orderItems = new Array();
-
-    let originalOrderID:string = "";
+    let originalOrderID = '';
 
     for (const order of orders) {
-
       if (order.original_order) {
         originalOrderID = order.original_order;
       }
 
-      let products = await get3TupleFromOrder(order.id);
+      const products = await get3TupleFromOrder(order.id);
 
-      let restaurant = await getRestaurantData(order.restaurant_id);
+      const restaurant = await getRestaurantData(order.restaurant_id);
 
       orderItems.push({
         items: products,
-        restaurant: (restaurant !== null) ? restaurant : {},
+        restaurant: restaurant !== null ? restaurant : {},
       });
-
     }
 
-    let newItems: Array<ItemWithLabInfo> = [];
+    const newItems: Array<ItemWithLabInfo> = [];
 
     for (const orderItem of orderItems) {
       for (const item of orderItem.items) {
+        const container: string = await getContainerNameByID(item.container_id);
 
-        let container: string = await getContainerNameByID(item.container_id);
-        
-        let itemFromDB = await getItemByID(item.item_id);
+        const itemFromDB = await getItemByID(item.item_id);
 
-        if (container !== null && itemFromDB != null) {
-          let newItem = {
+        if (container !== null && itemFromDB !== null) {
+          const newItem = {
             id: item.id,
             orderID: originalOrderID ? originalOrderID : item.order_id,
             restaurant: orderItem.restaurant.name,
@@ -174,61 +169,59 @@ export class OrderStore {
       }
     }
 
-    return newItems.sort((a:any, b:any)=>{
-      return a.priority - b.priority
+    return newItems.sort((a: ItemWithLabInfo, b: ItemWithLabInfo) => {
+      return a.priority - b.priority;
     });
   }
 
   public gatherItemsForLab(items: Array<ItemWithLabInfo>) {
-
-    let restaurants:Array<string> = []
+    const restaurants: Array<string> = [];
     for (const item of items) {
-      if(!restaurants.includes(item.restaurant)) {
+      if (!restaurants.includes(item.restaurant)) {
         restaurants.push(item.restaurant);
       }
     }
 
-    let itemsForEachRestaurant: Array<any> = []; 
-    let itemsProcessed: Array<string> = [];
-    
+    const itemsForEachRestaurant: Array<any> = [];
+    const itemsProcessed: Array<string> = [];
+
     for (const item of items) {
       if (!itemsProcessed.includes(item.name)) {
+        const quantities: Array<LabItemInfo> = [];
+        const containers: Array<LabItemInfo> = [];
 
-        let quantities:Array<LabItemInfo> = [];
-        let containers: Array<LabItemInfo> = [];
-        
         for (const itemToFind of items) {
           if (itemToFind.name === item.name) {
             quantities.push({
               item_order_id: itemToFind.id,
               quantity: itemToFind.quantity,
-              restaurant: itemToFind.restaurant
+              restaurant: itemToFind.restaurant,
             });
             containers.push({
               item_order_id: itemToFind.id,
               container: itemToFind.container,
-              restaurant: itemToFind.restaurant
+              restaurant: itemToFind.restaurant,
             });
           }
         }
 
         for (const restaurant of restaurants) {
-          let isInArray:boolean = false;
+          let isInArray = false;
           for (const quantity of quantities) {
             if (quantity.restaurant === restaurant) {
               isInArray = true;
-            } 
+            }
           }
           if (!isInArray) {
             quantities.push({
-              item_order_id: "",
+              item_order_id: '',
               quantity: 0,
-              restaurant: restaurant
+              restaurant: restaurant,
             });
             containers.push({
-              item_order_id: "",
-              container: "Aucun",
-              restaurant: restaurant
+              item_order_id: '',
+              container: 'Aucun',
+              restaurant: restaurant,
             });
           }
         }
@@ -238,7 +231,7 @@ export class OrderStore {
           name: item.name,
           quantities: quantities,
           containers: containers,
-          priority: item.priority
+          priority: item.priority,
         });
       }
 
@@ -246,7 +239,5 @@ export class OrderStore {
     }
 
     return itemsForEachRestaurant;
-
   }
 }
-
